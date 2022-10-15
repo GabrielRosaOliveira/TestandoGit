7a37374dad64988a93bb2999be38dfd6d041e993
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-15 19:30:09 -0300

    iniciei projeto 2, ao clicar no botao da tela principal, ele vai para segunda tela atraves de navigation

---
+++ test2/test2/Tela02VC.swift
@@ -0,0 +1,20 @@
+//
+//  Tela02VC.swift
+//  test2
+//
+//  Created by Gabriel on 15/10/22.
+//
+
+import UIKit
+
+class Tela02VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ test2/test2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Gabriel on 15/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func tappedScreen02Button(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
+                navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+    }
+}
+
