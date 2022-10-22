5cc1ef4225224f4f8df7fd7f58d4b2c0f32c3c1e
GabrielRosaOliveira <gabriel12rosaoliveira@gmail.com>
2022-10-22 09:20:28 -0300

    iniciando projeto tableView

---
+++ ProjetoLinkeIn/ProjetoLinkeIn/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ProjetoLinkeIn
+//
+//  Created by Gabriel on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
